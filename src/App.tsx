0ae363637eb81a6ebfdda45db6b1224002782a10
sihyonn <sihyeon982@naver.com>
2024-08-02 22:42:18 +0900

    chore: import 순서 자동정렬

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import { Global, ThemeProvider } from '@emotion/react';
 import globalStyle from '@/styles/globalStyle';
 import theme from '@/styles/theme';
-import { Global, ThemeProvider } from '@emotion/react';
 
 function App() {
   return (
