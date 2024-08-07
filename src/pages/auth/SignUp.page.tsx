6691744f3f02ab3976f59d667164a6c7db5216b3
sihyonn <sihyeon982@naver.com>
2024-08-07 14:45:08 +0900

    feat: 회원가입페이지 생성(임시)

---
+++ src/pages/auth/SignUp.page.tsx
@@ -0,0 +1,23 @@
+import Form from '@/components/common/form/Form';
+import FORM_INFO from '@/constants/FORM_INFO';
+import SignLayout from '@/styles/layout/SignLayout';
+
+export default function SignUp() {
+  return (
+    <SignLayout>
+      <Form
+        inputInformations={FORM_INFO.SIGN_UP}
+        onSubmit={() => console.log('제출')}
+        onError={() => console.log('폼 제출 실패')}
+        initialValues={{
+          email: '',
+          nickname: '',
+          password: '',
+          passwordCheck: '',
+        }}
+      >
+        <button>회원가입</button>
+      </Form>
+    </SignLayout>
+  );
+}
