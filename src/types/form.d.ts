a20870b66e51e8e62e17379545daff31af144771
sihyonn <sihyeon982@naver.com>
2024-08-07 14:43:08 +0900

    feat: form 관련 type 정의

---
+++ src/types/form.d.ts
@@ -0,0 +1,41 @@
+export interface FormValues {
+  [key: string]: any;
+}
+
+export interface InputInformations {
+  id: string;
+  type: string;
+  label?: string;
+  placeholder?: string;
+}
+
+export interface FormProps<T extends FormValues> {
+  inputInformations: readonly InputInformations[];
+  onSubmit: () => void;
+  onError?: () => void;
+  initialValues: T;
+  children?: React.ReactNode;
+}
+
+export type ValidateFunction<T extends FormValues> = (
+  id: keyof T,
+  value: string,
+  formData: T,
+) => string | undefined;
+
+export interface UseFormProps<T extends FormValues> {
+  initialValues: T;
+  validate: ValidateFunction<T>;
+}
+
+export interface UseFormReturn<T extends FormValues> {
+  formData: T;
+  errors: Record<string, string>;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+  handleSubmit: (
+    e: React.FormEvent<HTMLFormElement>,
+    onSubmit: (data: T) => void,
+  ) => void;
+  resetForm: () => void;
+}
