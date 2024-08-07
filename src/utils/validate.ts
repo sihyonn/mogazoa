8c832dce2d78d2e3788f03849486cabe1f9b452a
sihyonn <sihyeon982@naver.com>
2024-08-07 14:42:21 +0900

    feat: 회원가입 input 검증 함수 추가

---
+++ src/utils/validate.ts
@@ -0,0 +1,49 @@
+import { FormValues } from '@/types/form';
+import { ERROR_MESSAGE, FIELD_NAMES, REGEX } from '@/constants/FORM_VALIDATION';
+
+type FieldNamesType = typeof FIELD_NAMES;
+type FieldNameKey = keyof FieldNamesType;
+
+const checkRequiredField = (value: string, fieldName: string) => {
+  if (!value) return `${fieldName} 입력은 필수입니다!`;
+};
+
+const validateEmail = (value: string) => {
+  if (!REGEX.EMAIL.test(value)) return ERROR_MESSAGE.EMAIL;
+};
+
+const validateNickname = (value: string) => {
+  if (value.length > 20) return ERROR_MESSAGE.NICKNAME;
+};
+
+const validatePassword = (value: string) => {
+  if (value.length < 8) return ERROR_MESSAGE.PW_LENGTH;
+  if (!REGEX.PW.test(value)) return ERROR_MESSAGE.PW_FORMAT;
+};
+
+const validateConfirmPassword = (value: string, password: string) => {
+  if (value !== password) return ERROR_MESSAGE.PW_CHECK;
+};
+
+export const validateField = (
+  id: FieldNameKey,
+  value: string,
+  formData: FormValues,
+) => {
+  const fieldName = FIELD_NAMES[id];
+  const requiredError = checkRequiredField(value, fieldName);
+  if (requiredError) return requiredError;
+
+  switch (id) {
+    case 'email':
+      return validateEmail(value);
+    case 'nickname':
+      return validateNickname(value);
+    case 'password':
+      return validatePassword(value);
+    case 'passwordCheck':
+      return validateConfirmPassword(value, formData.password);
+    default:
+      return '';
+  }
+};
